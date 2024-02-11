35e253364bb3168acb7764e05a00d9f98516e87a
AndreaGul <andreagulino99@gmail.com>
2024-02-11 23:47:49 +0100

    feat: possibilita di associare una tipologia nella pagina di modifica di un progetto

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -53,7 +53,10 @@
     <select class="form-select " aria-label="Default select example" name="type_id">
       <option selected>Selezionane uno</option>
       @foreach ($types as $type)
-        <option value="{{$type->id}}">{{$type->title}}</option>
+         <option value="{{ $type->id }}" @if (old('type_id') == $type->id) selected @endif>
+       
+          {{$type->title}}
+        </option>
       @endforeach
     </select>
   </div>
